66fa533f63bb2f87cad19081065c7659d8b47167
LAKSHMINARAYANA <vinoth.lakshminarayana@capgemini.com>
2021-04-09 21:41:06 +0530

    updating changes

--- src/com/demo/home/HomePage.java
+++ src/com/demo/home/HomePage.java
@@ -2,7 +2,7 @@ package com.demo.home;
 
 public class HomePage {
 	public static void main(String[] args) {
-		System.out.println("HomePage edited");
+		System.out.println("HomePage edited from local");
 	}
 
 }
